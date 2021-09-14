ba62467edfd048871d9d313fd256f087638e2eb5
Harinder Grewal <harri@Harinders-MacBook-Pro.local>
2021-09-14 13:17:17 -0700

    new

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// constants
+const slotLookup = {
+    seven: {
+        imgUrl: 'imgs/7.jpeg',
+        beats: 'nothing'
+    }
+}
+
+console.log(slotLookup);
+
+const rpsLookup = {
+	rock: {
+		imgUrl: 'imgs/rock.png',
+		beats: 'scissors'
+	},
+	paper: {
+		imgUrl: 'imgs/paper.png',
+		beats: 'rock'
+	}, 
+	scissors: {
+		imgUrl: 'imgs/scissors.png',
+		beats: 'paper'
+	}
+}
+// app's state (varibales)
+// cached element references 
+// event listeners
+// functions
+// render
+// initialize 
+// initialize ()
+// render ();
+
